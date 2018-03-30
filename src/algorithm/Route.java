ba8aaeb9e7cd291fa46fa41b9bc2600467c3841c
Pheoda <n.fondacci@gmail.com>
2018-03-30 11:17:48 +0200

    Taboo fonctionnel, ne swap que parmi une seule route

--- src/algorithm/Route.java
+++ src/algorithm/Route.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Route {
@@ -37,5 +38,25 @@ public class Route {
 
     public void decreaseCapacityLeft(int quantity) {
         this.capacityLeft -= quantity;
+    }
+
+    public void swapClient(int i, int j) {
+        Collections.swap(route, i, j);
+    }
+
+    public float distanceRoute() {
+        float distanceRoute = 0;
+        Client firstClient = route.get(0);
+
+        for (int i = 1; i < route.size(); i++) {
+            distanceRoute += distance(firstClient.getX(), firstClient.getY(), route.get(i).getX(), route.get(i).getY());
+            firstClient = route.get(i);
+        }
+
+        return distanceRoute;
+    }
+
+    public float distance (int x1, int y1, int x2, int y2) {
+        return (float) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }
--- src/algorithm/Taboo.java
+++ src/algorithm/Taboo.java
@@ -1,24 +1,29 @@
 package algorithm;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Observer;
+import java.util.Random;
 
 public class Taboo extends Algorithm implements Runnable {
 
-    private final int SLEEPING_TIME = 500;
+    private final int SLEEPING_TIME = 5;
 
     private Graph graph;
 
     private Route currentRoute;
 
-    private Client[] switchedClient;
+    private int[] swappedClient;
+    private Route swappedRoute;
+
+    private Random randomGenerator;
 
     public Taboo(Graph graph, Observer observer) {
         super(observer);
         this.graph = graph;
         currentRoute = null;
-        switchedClient = new Client[2];
+        swappedClient = new int[2];
+
+        randomGenerator = new Random();
     }
 
     private void initRoutes() {
@@ -40,28 +45,52 @@ public class Taboo extends Algorithm implements Runnable {
     }
 
     private void closeAndNewRoute() {
-        if(currentRoute != null)
+        if (currentRoute != null)
             currentRoute.addClient(graph.getWarehouse());
         currentRoute = new Route();
         currentRoute.addClient(graph.getWarehouse());
     }
 
+    private float calculateDistance() {
+        float distance = 0;
+
+        for (Route r : routes)
+            distance += r.distanceRoute();
+
+        return distance;
+    }
+
     @Override
     protected void step() {
-        // échanger 2 points d'un chemin ssi ces 2 points ne sont pas dans la
+        float lastDistance = calculateDistance();
 
+        // Pick random route
+        swappedRoute = routes.get(randomGenerator.nextInt(routes.size()));
 
-        /*switchedClient[0] = ;
-        switchedClient[1] = ;*/
+        // Pick 2 random clients from route except the couple already switched
+        // First and last index are the warehouse !! Don't swap with these !!
+        swappedClient[0] = randomGenerator.nextInt(swappedRoute.getRoute().size() - 2) + 1;
+
+        int random;
+        do {
+            random = randomGenerator.nextInt(swappedRoute.getRoute().size() - 2) + 1;
+        }while(random == swappedClient[0]);
+        swappedClient[1] = random;
+
+        swappedRoute.swapClient(swappedClient[0], swappedClient[1]);
+
+        // Swap back if not worth
+        if (calculateDistance() > lastDistance)
+            swappedRoute.swapClient(swappedClient[0], swappedClient[1]);
+        //else
+        //    System.out.println("Stepped : swap " + swappedClient[0] + " and " + swappedClient[1] + " from " + swappedRoute + ", distance=" + lastDistance);
+        System.out.println("distance=" + calculateDistance());
     }
 
     @Override
     public void run() {
         initRoutes();
-        while(true) {
-            // Step taboo
-
-
+        while (true) {
             setChanged();
             notifyObservers();
             try {
@@ -69,6 +98,9 @@ public class Taboo extends Algorithm implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
+            // Step taboo
+            step();
         }
     }
 }
